d6974ca6650e955e6734732f16a6c848708f6d4a
ShashComandur <shashank.comandur@gmail.com>
2022-05-09 17:20:01 -0400

    Update generateImage.py

--- generateImage.py
+++ generateImage.py
@@ -1,32 +1,51 @@
-# Python program to take in a file, find its SHA256 hash, and turn it into a 2000x2000 truchet pattern png
-# Hashing function sourced from https://www.programiz.com/python-programming/examples/hash-file
+# Python program to take in a file, find its SHA512 hash, and turn it into a 2000x2000 truchet tile png
+# Hash function sourced from: https://www.programiz.com/python-programming/examples/hash-file
 
 # imports
 import hashlib
+import pygame
 
-# takes in an mp3, returns its SHA256
+# takes in an mp3, returns its SHA512 hash
 def hash_file(filename):
-
    # make a hash object
-   h = hashlib.sha256()
+   h = hashlib.sha512()
 
    # open file for reading in binary mode
    with open(filename,'rb') as file:
-
        # loop till the end of the file
        chunk = 0
        while chunk != b'':
            # read only 1024 bytes at a time
            chunk = file.read(1024)
            h.update(chunk)
-
    # return the hex representation of digest
-   return h.hexdigest()
+   #return bin(int(h.hexdigest(), 16))
+   return hextobin(h.hexdigest())
 
-def truchet(hash):
+# helper function to convert the hex string to a bin string
+def hextobin(h):
+  return bin(int(h, 16))[2:].zfill(len(h) * 4)
+
+# generate the 16x16 array to hold the bin substrings
+def generateArray(hash):
+    # initialize 16x16 double array to hold data for each tile
+    arr = [[""]*16 for i in range(16)]
+    stringIndex = 0
+
+    # populate array using the hash
+    for i in range(16):
+        for j in range(16):
+            arr[i][j] = hash[stringIndex: stringIndex+2]
+            stringIndex += 2
+    return arr
+
+def generateImage(arr):
 
     return
 
-# testing - move to another file soon
+# ------------------------------------------------ testing - move to another file soon 
 message = hash_file("../../Everything/Art/Posters/~resonance_system/Music Files/Noisia - Outer Edges/01 - The Approach.mp3")
 print("\nThe Approach: " + message + "\n")
+
+arr = generateArray(message)
+print(arr)
